889ba08ef20001185ae2f36f97b2fda08484166a
clown-now <clown-now@users.noreply.github.com>
2026-09-16 19:12:12 +0000

    fix: add import for getViewId extension

--- app/src/main/java/com/lu/wxmask/plugin/part/HideMainUIListPluginPart.kt
+++ app/src/main/java/com/lu/wxmask/plugin/part/HideMainUIListPluginPart.kt
@@ -10,6 +10,7 @@ import com.lu.lposed.api2.XC_MethodHook2
 import com.lu.lposed.api2.XposedHelpers2
 import com.lu.lposed.plugin.IPlugin
 import com.lu.magic.util.ResUtil
+import com.lu.wxmask.util.ext.getViewId
 import com.lu.magic.util.log.LogUtil
 import com.lu.magic.util.view.ChildDeepCheck
 import com.lu.wxmask.ClazzN
